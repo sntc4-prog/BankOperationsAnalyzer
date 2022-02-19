8fe34bd871ff62e70130499b4fde7d673b6ce0c1
Egor Borisov <EBorisov@luxoft.com>
2022-02-19 23:34:44 +0600

    Добавлены новые классы, использован принцип единственной ответственности и достигнута высокая связность

---
+++ src/main/java/BankStatementAnalyzer.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.List;
+
+public class BankStatementAnalyzer {
+    private static final String RESOURCES = "src/main/resources/";
+    private static final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
+
+    public static void main(String[] args) throws IOException {
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
+        final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+        collectSummary(bankStatementProcessor);
+    }
+
+    private static void collectSummary(BankStatementProcessor bankStatementProcessor) {
+        System.out.println("The total of all transactions is " + bankStatementProcessor.calculateTotalAmount());
+        System.out.println("The total of transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
+        System.out.println("The total of transactions in February is " + bankStatementProcessor.calculateTotalInMonth(Month.FEBRUARY));
+        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+    }
+}
