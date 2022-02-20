e98c4c19cb89f87b63884affff9bbe9798e4e89c
Egor Borisov <EBorisov@luxoft.com>
2022-02-20 19:44:03 +0600

    Added BankStatementProcessorTest

--- src/test/java/ru/borisov/bank/BankStatementParserTest.java
+++ src/test/java/ru/borisov/bank/BankStatementParserTest.java
@@ -2,10 +2,16 @@ package ru.borisov.bank;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 public class BankStatementParserTest {
+    private static final String RESOURCES = "src/main/resources/";
     private final BankStatementParser statementParser = new BankStatementCSVParser();
 
     @Test
@@ -17,5 +23,17 @@ public class BankStatementParserTest {
         Assert.assertEquals(expected.getDate(),result.getDate());
         Assert.assertEquals(expected.getAmount(),result.getAmount(),tolerance);
         Assert.assertEquals(expected.getDescription(),result.getDescription());
+    }
+    @Test
+    public void BankStatementProcessorTest() throws IOException {
+        final double expectedTotalAmount = 6820.0;
+        final String fileName = "bank-data-simple.csv";
+        final Path path = Paths.get(RESOURCES + fileName);
+        final List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = statementParser.parseLinesFrom(lines);
+        BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
+        final double tolerance = 0.0d;
+        Assert.assertEquals(expectedTotalAmount,bankStatementProcessor.calculateTotalAmount(),tolerance);
     }
 }
